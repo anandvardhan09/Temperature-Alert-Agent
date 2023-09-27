45f82c93fb6652408678a9befae23ad3dafca591
Suraj Sarkar <37933420+surajsarkar@users.noreply.github.com>
2023-09-27 13:59:03 +0000

    activate virtual env

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 import os
 import time
+import platform
+
+current_os = platform.system().lower()
+py = 'python3' if current_os == "linux" else 'python'
 if not os.path.exists("./hackAI"):
     print("Creating Virtual Environment.")
-    os.system("python3 -m venv hackAI")
+    os.system(f"{py} -m venv hackAI")
 else:
     print("Virtual Environment Exists Skipping Creation.\n")
 
 os.system("clear")
+
+activate_command = ""
+if platform == "windows":
+    activate_command = "hackAI\Scripts\activate"
+elif platform == "linux":
+    activate_command = "source hackAI/bin/activate"
+
+# activate pyenv
+os.system(activate_command)
 
 if not os.path.exists("./requirements.txt"):
     print("requirements.txt file not found")
@@ -16,4 +29,4 @@ else:
 print("\nSetup Complete\nStarting the App.")
 time.sleep(5)
 os.system("clear")
-os.system("python3 ./src/main.py")
+os.system(f"{py} ./src/main.py")
